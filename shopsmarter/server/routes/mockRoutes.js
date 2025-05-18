0de9303e56cc3d567fefc94c94814e67f4156e7b
Hemanth-2706 <ee24b024@smail.iitm.ac.in>
2025-05-18 21:22:08 +0530

    Home Page 1.0
    Home page 1.0 is done
    Banner Images added

--- shopsmarter/server/routes/mockRoutes.js
+++ shopsmarter/server/routes/mockRoutes.js
@@ -1,32 +1,47 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 const router = express.Router();
 
 const products = [
 	{
 		_id: "1",
-		name: "Stylish Jacket",
-		description: "Warm and stylish",
-		price: 59.99,
-		image: "https://via.placeholder.com/150",
+		name: "Fastrack Smartwatch",
+		description: "Stylish and functional",
+		price: 1399,
+		image: "/images/watch.png",
 	},
 	{
 		_id: "2",
-		name: "Cool Sneakers",
-		description: "Trendy sneakers",
-		price: 49.99,
-		image: "https://via.placeholder.com/150",
+		name: "Men Regular Fit Self Design Built-up Collar Casual Shirt",
+		description: "Comfortable and stylish",
+		price: 355,
+		image: "/images/shirt.png",
 	},
 	{
 		_id: "3",
-		name: "Desk Lamp",
-		description: "Modern lamp",
-		price: 24.99,
-		image: "https://via.placeholder.com/150",
+		name: "Shoes",
+		description: "Comfortable and stylish shoes",
+		price: 1499,
+		image: "/images/shoes.png",
 	},
 ];
 
+// Home Route - loads product data and banner images
 router.get("/", (req, res) => {
-	res.render("home", { products });
+	const bannerDir = path.join(
+		__dirname,
+		"../../client/public/images/banner"
+	);
+	let bannerImages = [];
+
+	try {
+		bannerImages = fs.readdirSync(bannerDir);
+	} catch (err) {
+		console.error("Error reading banner images folder:", err);
+	}
+
+	res.render("home", { products, bannerImages });
 });
 
 router.get("/recommend", (req, res) => {
